1f49889d8c861f443d29cc3a7a4bde37d225df4b
Slipinir <quilinho.junior@gmail.com>
2022-07-02 13:08:42 -0300

    *breve explicação do integrador

--- Main/Integrador.dpr
+++ Main/Integrador.dpr
@@ -5,8 +5,14 @@ program Integrador;
 uses
   SysUtils;
 
+var
+  I: Integer;
 begin
   try
+    Writeln('Aqui as classes serao instanciadas e utilizadas para importar os pedidos, clientes e etc');
+    Writeln('Pode dividir em N executaveis para que cada um importe apenas os pedidos, clientes ou produtos');
+    Writeln('Realizar a divisão dos processos pode dividir a carga de trabalho entre os executaveis');
+    Readln(I);
     { TODO -oUser -cConsole Main : Insert code here }
   except
     on E:Exception do
